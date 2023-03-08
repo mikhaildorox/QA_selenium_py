ba80eb33f2fbc69cc7910870e4785729ae4c9a34
mikhaildoroshenko <mixdorosh@gmail.com>
2023-03-08 22:20:01 +0700

    complete lesson6 step4 and step5

--- lesson6_step4.py
+++ lesson6_step4.py
@@ -7,7 +7,6 @@ link = "http://suninjuly.github.io/simple_form_find_task.html"
 try:
     browser = webdriver.Chrome()
     browser.get(link)
-
     input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
     input2 = browser.find_element(By.NAME, "last_name")
