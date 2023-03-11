00758c1b40d6be5719558ee15454ac7f2d9c4a9f
mikhaildoroshenko <mixdorosh@gmail.com>
2023-03-12 01:19:36 +0700

    complete lesson2.1 step7

---
+++ lesson2/21_step7.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    get_treasure = browser.find_element(By.ID, "treasure").get_attribute("valuex")
+    answer = calc(get_treasure)
+    browser.find_element(By.ID, 'answer').send_keys(answer)
+    browser.find_element(By.ID, 'robotCheckbox').click()
+    browser.find_element(By.ID, 'robotsRule').click()
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
